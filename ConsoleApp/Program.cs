8c7cbb7ad6ee38f2351918d94098d54a96cccb11
Awais Ilyas <im.awaisilyas@gmail.com>
2020-12-17 11:32:46 +0400

    ConsoleApp project created to call Client using HttpClient

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            swaggerClient client = new("https://localhost:44367", new HttpClient()); // quick and dirty way to setup HttpClient
+
+            var records = await client.WeatherForecastAsync();
+
+
+            foreach (var r in records)
+            {
+                Console.WriteLine($"{r.Date}: {r.Summary}");
+            }
+        }
+    }
+}
